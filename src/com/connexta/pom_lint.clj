f5a86b5d06b1f1cf6afb11c5f2f35a7e46718e1c
R.A. Porter <richard.porter@connexta.com>
2020-01-23 12:12:04 -0700

    First test in place.

--- src/com/connexta/pom_lint.clj
+++ src/com/connexta/pom_lint.clj
@@ -12,42 +12,45 @@
         (for [n (apply xml-> pomfile p)]
           (zip/node n))))
 
+(defn xml->depstruct [dep]
+  (->> dep
+       (map (juxt :tag :content))
+       (into {})))
+
 (defn extract-deps [deplst]
-  (let [baz (fn [dep]
-              (->> dep
-                   (map #(map % [:tag :content]))
-                   (map vec)
-                   vec
-                   (into {})))]
     (->> deplst
          (map :content)
-         (map baz)
-         set
-         )))
+         (map xml->depstruct)
+         set))
 
 (defn missing-deps [root-deps plugin-deps]
   (remove root-deps plugin-deps))
 
-  (defn miss [p]
-    (let [content
-          (fn [x]
-            (reduce-kv (fn [m k v]
-                         (conj m {:tag k :content v}))
-                       []
-                       x))]
-      (xml/emit-element {:tag     :dependency
-                         :content (content p)})))
+(defn miss [p]
+  (let [content
+        (fn [x]
+          (reduce-kv (fn [m k v]
+                       (conj m {:tag k :content v}))
+                     []
+                     x))]
+    (xml/emit-element {:tag     :dependency
+                       :content (content p)})))
 
 
-(def pomfile (read-pom "/opt/projects/cx/pomfix/resources/pom.xml"))
-(def root-dependencies (build-node-list pomfile :dependencies :dependency))
-(def root-plugin-dependencies (build-node-list pomfile :build :plugins :plugin :dependencies :dependency))
-(def profile-plugin-dependencies (build-node-list pomfile :profiles :profile :build :plugins :plugin :dependencies :dependency))
+(defn main [fname]
+  (let [pomfile (read-pom fname)
+        root-dependencies (build-node-list pomfile :dependencies :dependency)
+        root-plugin-dependencies (build-node-list pomfile :build :plugins :plugin :dependencies :dependency)
+        profile-plugin-dependencies (build-node-list pomfile :profiles :profile :build :plugins :plugin :dependencies :dependency)
+        root-art-dependencies (build-node-list pomfile :build :plugins :plugin :executions :execution :configuration :artifactItems :artifactItem)
+        profile-art-dependencies (build-node-list pomfile :profiles :profile :build :plugins :plugin :executions :execution :configuration :artifactItems :artifactItem)
+        root-deps (extract-deps root-dependencies)
+        plugin-deps (concat (extract-deps root-plugin-dependencies)
+                            (extract-deps profile-plugin-dependencies)
+                            (extract-deps root-art-dependencies)
+                            (extract-deps profile-art-dependencies))
+        missing (missing-deps root-deps plugin-deps)]
+    missing))
 
-(def root-deps (extract-deps root-dependencies))
-(def plugin-deps (concat (extract-deps root-plugin-dependencies) (extract-deps profile-plugin-dependencies)))
-
-(def missing (missing-deps root-deps plugin-deps))
-
-
-; (map miss missing)
+(comment
+  (main "/opt/projects/cx/pomfix/resources/pom.xml"))
---
+++ test/com/connexta/pom_lint_test.clj
@@ -0,0 +1,12 @@
+(ns com.connexta.pom-lint-test
+  (:require [com.connexta.pom-lint :as pl]
+            [clojure.test :refer [deftest is]]
+            [clojure.java.io :as io]))
+
+(deftest pom-missing-dependency
+  (is (= (pl/main (.getPath (io/resource "artifact-items/pom.xml")))
+         [{:groupId ["ddf.lib"],
+           :artifactId ["grunt-port-allocator"],
+           :version ["${project.version}"],
+           :type ["tar.gz"],
+           :classifier ["npm"]}])))
