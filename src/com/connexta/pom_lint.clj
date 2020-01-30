612bd17b6725a4b7f243d6e66b721a25b8624e97
Ryan Zwiefelhofer <ryan.zwiefelhofer@connexta.com>
2020-01-30 13:07:55 -0700

    Added support for descriptors

--- src/com/connexta/pom_lint.clj
+++ src/com/connexta/pom_lint.clj
@@ -1,12 +1,19 @@
 (ns com.connexta.pom-lint
   (:require [clojure.pprint :refer [pprint]]
             [clojure.xml :as xml]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.string :as str]))
 
 (defn xml->depstruct [dep]
   (->> dep
        (map (juxt :tag :content))
        (into {})))
+
+(defn descriptor->depstruct [match]
+  (let [[_ groupid artifactid version] match]
+    {:groupId    [groupid]
+     :artifactId [artifactid]
+     :version    [version]}))
 
 (defn get-root-deps [data]
   (->> (:content data)
@@ -24,20 +31,32 @@
        (map xml->depstruct)
        set))
 
+(defn get-all-descriptors [data]
+  (->> (xml-seq data)
+       (filter #(= :descriptor (:tag %)))
+       (map :content)
+       first
+       (map str/trim)
+       (map #(re-matches #"mvn:(.*)\/(.*)\/(.*)\/xml\/features" %))
+       (map descriptor->depstruct)
+       set))
+
 (defn main [fname]
   (let [data (xml/parse fname)
         root-deps (get-root-deps data)
-        all-deps (get-all-deps data)]
+        all-deps (into (get-all-deps data) (get-all-descriptors data))]
     (set/difference all-deps root-deps)))
 
 (comment
   (def data (xml/parse (.getPath (clojure.java.io/resource "artifact-items/pom.xml"))))
+  (def data (xml/parse (.getPath (clojure.java.io/resource "descriptors/pom.xml"))))
   (def data (xml/parse "/opt/git/ddf/pom.xml"))
-
   (get-root-deps data)
   (get-all-deps data)
+  (get-all-descriptors data)
 
-  (main (.getPath (clojure.java.io/resource "artifact-items/pom.xml"))))
+  (main (.getPath (clojure.java.io/resource "artifact-items/pom.xml")))
+  (main (.getPath (clojure.java.io/resource "descriptors/pom.xml"))))
 ; This will generate the dependency xml to be added.
 
 
--- test/com/connexta/pom_lint_test.clj
+++ test/com/connexta/pom_lint_test.clj
@@ -1,12 +1,19 @@
 (ns com.connexta.pom-lint-test
   (:require [com.connexta.pom-lint :as pl]
-            [clojure.test :refer [deftest is]]
+            [clojure.test :refer [deftest is are]]
             [clojure.java.io :as io]))
 
 (deftest pom-missing-dependency
-  (is (= (pl/main (.getPath (io/resource "artifact-items/pom.xml")))
-         #{{:groupId    ["ddf.lib"],
-            :artifactId ["grunt-port-allocator"],
-            :version    ["${project.version}"],
-            :type       ["tar.gz"],
-            :classifier ["npm"]}})))
+  (are [file-name missing-deps]
+    (= (pl/main (.getPath (io/resource file-name)))
+       missing-deps)
+    "artifact-items/pom.xml"
+    #{{:groupId    ["ddf.lib"],
+       :artifactId ["grunt-port-allocator"],
+       :version    ["${project.version}"],
+       :type       ["tar.gz"],
+       :classifier ["npm"]}}
+    "descriptors/pom.xml"
+    #{{:groupId    ["ddf.features"]
+       :artifactId ["install-profiles"]
+       :version    ["${project.version}"]}}))
